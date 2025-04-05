d6d9f78eb5e797a6dcae536bb305431e326b6daa
David Williams <ddwilliams74@gmail.com>
2025-04-05 02:58:31 -0400

    task3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,14 +19,17 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
-  return res.status(300).json({message: "Yet to be implemented isbn"});
+    return res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented author"});
+    const author = req.params.author;
+    for (book in books) {
+        if (books[book]["author"] === String(author)) {
+            return res.send(books[book]);
+        }
+    }
 });
 
 // Get all books based on title
