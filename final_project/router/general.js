656a422ab0c24b2ff5957ba9421901b71f3bf9be
David Williams <ddwilliams74@gmail.com>
2025-04-05 17:17:33 -0400

    task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,13 +4,27 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+//let users = []
 
-public_users.post("/register", (req,res) => {
-    // Register a new user
-    app.post("/register", (req, res) => {
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Register a new user`
+public_users.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    
+
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
@@ -19,13 +33,13 @@ public_users.post("/register", (req,res) => {
             users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
         } else {
-            return res.status(404).json({message: "User already exists!"});
+        return res.status(404).json({message: "User already exists!"});
         }
     }
     // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
-    });
+    return res.status(404).json({message: "Unable to register user "});
 });
+
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
